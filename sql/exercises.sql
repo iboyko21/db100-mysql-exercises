5027c92e27336c2aee12da9a2631d64dedc9eb11
Igor Boyko <iboyko@gmail.com>
2022-12-13 16:37:58 -0800

    6 tests passed

--- sql/exercises.sql
+++ sql/exercises.sql
@@ -13,10 +13,12 @@
 ## 1. SELECT statements
 
 # 1a. Select all columns from the actor table.
+
 SELECT * FROM actor;
 
 
 # 1b. Select only the last_name column from the actor table.
+
 SELECT last_name FROM actor;
 
 
@@ -43,14 +45,17 @@ FROM film;
 ## 2. DISTINCT operator
 
 # 2a. Select all distinct (different) last names from the actor table.
+
 SELECT DISTINCT last_name FROM actor;
 
 
 # 2b. Select all distinct (different) postal codes from the address table.
+
 SELECT DISTINCT postal_code FROM address;
 
 
 # 2c. Select all distinct (different) ratings from the film table.
+
 SELECT DISTINCT rating FROM film;
 
 # ---------------------------------------------------------#
@@ -59,34 +64,42 @@ SELECT DISTINCT rating FROM film;
 ## 3. WHERE clause
 
 # 3a. Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
+
 SELECT title, description, rating, movie_length FROM films WHERE movie_length >= 180;
 
 
 # 3b. Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
+
 SELECT payment_id, amount, payment_date FROM payment WHERE payment_date > '2005-05-27';
 
 
 # 3c. Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
+
 SELECT primary_key, amount, payment_date FROM payment WHERE payment_date > '2005-05-27';
 
 
 # 3d. Select all columns from the customer table for rows that have a last name beginning with "S" and a first name ending with "N".
+
 SELECT * from customer WHERE last_name LIKE 'S%' and first_name LIKE 'N%';
 
 
 # 3e. Select all columns from the customer table for rows where the customer is inactive or has a last name ending with "M".
+
 SELECT * FROM customer WHERE active = 0 OR last_name LIKE '%M';
 
 
 # 3f. Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either "C", "S" or "T".
+
 SELECT * from category WHERE category_id > 4 AND name LIKE 'C%' OR 'S%' OR 'T%';
 
 
 # 3g. Select all columns minus the password column from the staff table for rows that contain a password.
+
 SELECT staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update FROM staff WHERE password IS NOT NULL;
 
 
 # 3h. Select all columns minus the password column from the staff table for rows that do not contain a password.
+
 SELECT staff_id, first_name, last_name, address_id, picture, email, store_id, username, last_update FROM staff WHERE password IS NULL;
 
 
@@ -96,15 +109,18 @@ SELECT staff_id, first_name, last_name, address_id, picture, email, store_id, us
 ## 4. IN operator
 
 # 4a. Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java.
+
 SELECT phone, district FROM address WHERE district IN ('California', 'England', 'Taipei', 'West Java');
 
 
 # 4b. Select the payment id, amount, and payment date columns from the payment table for payments made on 05/25/2005, 05/27/2005, and 05/29/2005.
 # (Use the IN operator and the DATE function, instead of the AND operator as in previous exercises.)
+
 SELECT * FROM payment WHERE DATE(payment_date) IN ('2005-05-25', '2005-05-27', '2005-05-29');
 
 
 # 4c. Select all columns from the film table for films rated G, PG-13 or NC-17.
+
 SELECT * FROM film WHERE rating IN ('G', 'PG-13', 'NC-17');
 
 
@@ -114,6 +130,7 @@ SELECT * FROM film WHERE rating IN ('G', 'PG-13', 'NC-17');
 ## 5. BETWEEN operator
 
 # 5a. Select all columns from the payment table for payments made between midnight 05/25/2005 and 1 second before midnight 05/26/2005.
+
 SELECT * FROM payment WHERE payment_date BETWEEN '2005-05-25 00:00:00' AND '2005-05-26 11:59:59';
 
 
@@ -125,7 +142,12 @@ SELECT * FROM payment WHERE payment_date BETWEEN '2005-05-25 00:00:00' AND '2005
 # release_year          Exists in film table.
 # total_rental_cost     rental_duration * rental_rate
 
-SELECT title, description, release_year, rental_duration * rental_rate as total_rental_cost FROM film WHERE CHAR_LENGTH(description) BETWEEN '100' AND '120';
+SELECT 
+    title, 
+    description, 
+    release_year, 
+    rental_duration * rental_rate as total_rental_cost 
+FROM film WHERE CHAR_LENGTH(description) BETWEEN '100' AND '120';
 
 
 # ---------------------------------------------------------#
@@ -136,13 +158,19 @@ SELECT title, description, release_year, rental_duration * rental_rate as total_
 # 6a. Select the following columns from the film table for rows where the description begins with "A Thoughtful".
 # Title, Description, Release Year
 
+SELECT title, description, release_year FROM film WHERE description LIKE 'A Thoughtful%';
+
 
 # 6b. Select the following columns from the film table for rows where the description ends with the word "Boat".
 # Title, Description, Rental Duration
 
+SELECT title, description, rental_duration FROM film WHERE description LIKE '%Boat';
+
 
 # 6c. Select the following columns from the film table where the description contains the word "Database" and the length of the film is greater than 3 hours.
 # Title, Length, Description, Rental Rate
+
+SELECT title, length, description, rental_rate FROM film WHERE description LIKE '%Database%' and length >= '180';
 
 
 # ---------------------------------------------------------#
